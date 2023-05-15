30559af4c897e7fdd660b62cccdd0e4bb563c3a4
Eiren Jacobson <eiren.jacobson@gmail.com>
2023-05-15 18:28:30 +0100

    Fixed simCapRecap so only subadults and adults can be catalogued

--- Scripts/simCapRecap.R
+++ Scripts/simCapRecap.R
@@ -3,10 +3,12 @@
 
 simCapRecap <- function(Zmat, pcap){
   
+  ASA <- 5
+  AMAX <- 50
   # Zmat is the population matrix with nrow individuals and nyear columns
   # Pcap is the capture probability 
-
-  alive <- Zmat[which(Zmat[,ncol(Zmat)] == 1),]
+  ages <- rowSums(Zmat)
+  alive <- Zmat[which(Zmat[,ncol(Zmat)] == 1 & ages %in% ASA:AMAX),]
   
   IDs <- dimnames(alive[which(rbinom(n = nrow(alive), size = 1, prob = pcap) == 1),])[[1]]
   
