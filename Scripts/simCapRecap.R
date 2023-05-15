9ba4301cb6134721456479105451220ad62c1f2e
Eiren Jacobson <eiren.jacobson@gmail.com>
2023-05-15 10:56:54 +0100

    Wrote generic survey simulation function

--- Scripts/simCapRecap.R
+++ Scripts/simCapRecap.R
@@ -1,14 +1,14 @@
 
 # Script to simulate one year of capture-recapture data
 
-simCapRecap <- function(Zmat, Pcap){
+simCapRecap <- function(Zmat, pcap){
   
   # Zmat is the population matrix with nrow individuals and nyear columns
   # Pcap is the capture probability 
 
   alive <- Zmat[which(Zmat[,ncol(Zmat)] == 1),]
   
-  IDs <- dimnames(alive[which(rbinom(n = nrow(alive), size = 1, prob = Pcap) == 1),])[[1]]
+  IDs <- dimnames(alive[which(rbinom(n = nrow(alive), size = 1, prob = pcap) == 1),])[[1]]
   
   return(IDs)
   
---
+++ Scripts/simSurvey.R
@@ -0,0 +1,32 @@
+
+# generic function to simulate data collection given true N and expected CV
+# returns a single survey estimate NEst drawn from a lognormal distribution
+
+simSurvey <- function(N, CV){
+  
+  # N is (true/simulated) population size, assumed to be estimated without bias
+  # CV is the expected CV of a survey
+  
+  #N <- 100
+  #CV <- 0.2
+  
+  # the target mean and sd on the real scale
+  mu_X <- N
+  sigma_X <- CV*N
+  
+  # transform to mean and sd on the log scale
+  mu <- log(mu_X^2/sqrt(mu_X^2 + sigma_X^2))
+  sigma <- sqrt(log(1+(sigma_X^2/mu_X^2)))
+  
+  # check that coverage matches expected CIs
+  # C <- exp(1.96 * sqrt(log(1+CV^2)))
+  # LCI <- N/C
+  # UCI <- N*C
+  # samples <- rlnorm(10000, meanlog = mu, sdlog = sigma)
+  # length(samples[which(samples>LCI & samples<UCI)])/length(samples)
+  
+  NEst <- rlnorm(1, meanlog = mu, sdlog = sigma)
+  
+  return(NEst)
+  
+}
