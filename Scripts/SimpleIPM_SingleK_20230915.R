c0c2674789c18311c2731d5f8ebec6b33f56163e
Eiren Jacobson <eiren.jacobson@gmail.com>
2023-09-15 18:07:09 +0100

    Simple IPMs with deterministic simulation, single region, no change in K

---
+++ Scripts/SimpleIPM_SingleK_20230915.R
@@ -0,0 +1,82 @@
+library(nimble)
+library(runjags)
+library(ggplot2)
+
+
+source("./Scripts/simPop.R")
+source("./Scripts/simSurvey.R")
+source("./Scripts/nimbleIPM_Simple_SingleK_20230915.R")
+
+targetN <- 500
+nyears <- 100
+ltcv <- 0.35
+pamcv <- 0.15
+
+pop <- simPop(K = targetN, nyears = nyears)
+
+N.true <- rowSums(pop$N)
+
+LT.data <- vector()
+PAM.data <- vector()
+for (i in 1:100){
+  LT.data[i] <- simSurvey(N.true[i], ltcv)
+  PAM.data[i] <- simSurvey(N.true[i], pamcv)
+}
+
+Ndefault <- simPop(K = targetN, nyears = nyears)  
+
+nimbleData <- list(ltestN = LT.data, ltestSD = LT.data*ltcv, 
+                   pamestN = PAM.data, pamestSD = PAM.data*pamcv)
+
+nimbleConstants <- list(cyear = 50, nyears = nyears, S0 = 0.85, S1 = 0.9, AFR = 10,
+                        AJU = 3, ASA = 5, AMAX = 50, fmax = 0.2,  
+                        z = 2.39, 
+                        ltyears = 1:nyears, nltyears = length(1:nyears),
+                        pamyears = 1:nyears, npamyears = length(1:nyears),
+                        K_lower = 400,
+                        K_upper = 600)
+
+nimbleInits <- list(K1_scalar = 1, 
+                    N = round(Ndefault$N), Ntot = round(rowSums(round(Ndefault$N))))
+
+nimbleParams <- list("K1",  "noncalves")
+
+set.seed(20230915)
+
+for (i in 1:10){
+
+model <- nimbleModel(code = ipm,
+                     constants = nimbleConstants,
+                     data = nimbleData,
+                     inits = nimbleInits,
+                     check = FALSE)
+
+# Run the model
+nimbleOut <- nimbleMCMC(model, 
+                        monitors = nimbleParams, 
+                        constants = nimbleConstants, data = nimbleData,
+                        thin = 10, niter = 50000, nburnin = 40000, nchains = 4,
+                        samplesAsCodaMCMC = TRUE)
+
+results[[i]] <- nimbleOut
+
+}
+
+K.results <- data.frame("i" = 1:10, "K1" = NA)
+N.results <- data.frame()
+
+for (i in 1:10){
+  
+  single.mcmc <- combine.mcmc(results[[i]])
+  
+  K.results[i,] <- c(i, summary(single.mcmc)$quantiles[1,3])
+  N.results <- rbind.data.frame(N.results, data.frame("i" = i, "Year" = 1:(nyears-1), 
+                          "Noncalves" = summary(single.mcmc)$quantiles[2:100,3]))
+  
+}
+
+ggplot()+
+  geom_line(data = N.results, aes(x=Year, y = Noncalves, group = i)) +
+  geom_segment(aes(x = 0, xend = 100, y = K.results$K1, yend = K.results$K1), color = "blue") 
+
+
---
+++ Scripts/nimbleIPM_Simple_20230915.R
@@ -0,0 +1,80 @@
+
+ipm <- nimbleCode({
+  
+  # Fixed values
+  
+  # Priors for parameters to be estimated by the model
+
+  K1_scalar ~ dunif(0, 1)
+  K2_scalar ~ dunif(0, 1)
+  
+  S2 <- 0.95
+  K1 <- K_lower + K1_scalar*(K_upper-K_lower)
+  K2 <- K_lower + K2_scalar*(K_upper-K_lower)
+  
+  # Process model
+  
+  # construct a vector of survival probabilities
+  S[1:AJU] <- S0
+  S[(AJU+1):ASA] <- S1
+  S[(ASA+1):(AMAX-1)] <- S2
+  S[AMAX] <- 0
+  
+  # construct a vector of carrying capacity
+  K[1:nyears] <- c(rep(K1, cyear), rep(K2, nyears-cyear))
+  
+  # Calculate fec at equilibrium according to Eq 3 in Brandon et al
+  # Density-dependence affects fecundity according to Pella-Tomlinson
+  f0 <- (1-S2) / ( (S0^AJU) * (S1^(ASA-AJU)) * (S2^(AFR-ASA)) * (1-(S2^(AMAX-AFR))) )
+  # Calculate proportion in each age class at equilibrium
+  
+  P[1] <- 1
+  
+  for(i in 2:(AMAX+1)){
+    P[i] <- prod(S[1:(i-1)])
+  }
+  
+  # distribute K1 individuals according to P
+  R0 <- K1/sum(P[2:(AMAX+1)])
+  for (i in 1:AMAX){
+    N[1, i] <- P[i]*R0
+  }
+  Ntot[1] <- sum(N[1, 1:AMAX])
+
+  # run population forward in time
+  for (t in 1:(nyears-1)){
+    calves[t] <- N[t, 1]
+    noncalves[t] <- sum(N[t, 2:(AMAX+1)])
+    ft[t] <- max(0, (f0 + (fmax-f0)*(1-(sum(N[t, 2:(AMAX+1)])/K[t])^z))) # fec at t
+    #N[t+1, 1] ~ dbin(ft[t], round(sum(N[t, AFR:AMAX]))) # calves
+    N[t+1, 1] <- sum(N[t, (AFR+1):(AMAX+1)])*ft[t]
+    for (a in 2:(AMAX+1)){
+      #N[t+1, a] ~ dbin(S[a-1], N[t, a-1]) # juv and adult 
+      N[t+1, a] <- S[a-1]*N[t, a-1]
+    } # end for a
+    Ntot[t+1] <- sum(N[t+1, 1:(AMAX+1)])
+  } # end for t
+  
+  # Visual survey observation model
+  # including calves
+  
+  for (t in 1:nltyears){
+    trueN[t] <- Ntot[ltyears[t]]
+     #mu_log_lt[t] <- log(trueN[t]^2/sqrt(trueN[t]^2 + ltestSD[t]^2))
+     #sigma_log_lt[t] <- sqrt(log(1+(ltestSD[t]^2/trueN[t]^2)))
+     #ltestN[t] ~ dlnorm(meanlog = mu_log_lt[t], sdlog = sigma_log_lt[t])
+    ltestN[t] ~ dlnorm(meanlog = log(trueN[t]), sdlog = log(ltestSD[t]))
+  }
+  
+  # Passive acoustic survey
+
+  for (t in 1:npamyears){
+    trueNb[t] <- Ntot[pamyears[t]]/2
+    #mu_log_pam[t] <- log(trueNb[t]^2/sqrt(trueNb[t]^2 + pamestSD[t]))
+    #sigma_log_pam[t] <- sqrt(log(1+(pamestSD[t]^2/trueNb[t]^2)))
+    #pamestN[t] ~ dlnorm(meanlog = mu_log_pam[t], sdlog = sigma_log_pam[t])
+    pamestN[t] ~ dlnorm(meanlog = log(trueNb[t]), sdlog = log(pamestSD[t]))
+  }
+
+
+})
