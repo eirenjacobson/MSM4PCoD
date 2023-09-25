e348b67c42c09729067ab11bc0db4646361ab3c6
Eiren Jacobson <eiren.jacobson@gmail.com>
2023-09-25 11:16:30 +0100

    Rerun single K model with trace on calves and noncalves

--- Scripts/SimpleIPM_SingleK_20230915.R
+++ Scripts/SimpleIPM_SingleK_20230915.R
@@ -16,7 +16,7 @@ set.seed(20230920)
 
 results <- list()
 
-for (i in 1:1){
+for (i in 1:10){
 
 pop <- simPop(K = targetN, nyears = nyears)
 
@@ -45,7 +45,7 @@ nimbleConstants <- list(nyears = nyears, S0 = 0.85, S1 = 0.9, AFR = 10,
 nimbleInits <- list(K1_scalar = 1, 
                     N = round(Ndefault$N), Ntot = round(rowSums(round(Ndefault$N))))
 
-nimbleParams <- list("K1",  "noncalves")
+nimbleParams <- list("K1",  "noncalves", "calves")
 
 
 model <- nimbleModel(code = ipm,
@@ -58,29 +58,50 @@ model <- nimbleModel(code = ipm,
 nimbleOut <- nimbleMCMC(model, 
                         monitors = nimbleParams, 
                         constants = nimbleConstants, data = nimbleData,
-                        thin = 10, niter = 50000, nburnin = 40000, nchains = 1,
+                        thin = 10, niter = 50000, nburnin = 40000, nchains = 4,
                         samplesAsCodaMCMC = TRUE)
 
 results[[i]] <- nimbleOut
 
 }
 
-K.results <- data.frame("i" = 1:10, "K1" = NA)
+K.results <- data.frame("i" = 1:10, "K1_Med" = NA, "K1_LCI" = NA, "K1_UCI" = NA)
 N.results <- data.frame()
 
-for (i in 1:1){
+for (i in 1:10){
   
   single.mcmc <- combine.mcmc(results[[i]])
   
-  K.results[i,] <- c(i, summary(single.mcmc)$quantiles[1,3])
+  K.results[i,] <- c(i, summary(single.mcmc)$quantiles[1,3], 
+                     summary(single.mcmc)$quantiles[1,1],
+                     summary(single.mcmc)$quantiles[1,5])
   N.results <- rbind.data.frame(N.results, data.frame("i" = i, "Year" = 1:(nyears-1), 
-                          "Noncalves" = summary(single.mcmc)$quantiles[2:100,3]))
+                          "calves" = summary(single.mcmc)$quantiles[2:100,3],
+                          "noncalves" = summary(single.mcmc)$quantiles[101:199, 3]))
   
 }
 
-ggplot()+
-  geom_line(data = N.results, aes(x=Year, y = Noncalves, group = i)) +
-  geom_segment(aes(x = 0, xend = 100, y = K.results$K1, yend = K.results$K1), color = "blue")+
+ggplot(data = N.results)+
+  geom_line(aes(x=Year, y = noncalves, group = i)) +
+  geom_line(aes(x=Year, y = noncalves+calves, group = i))+
+  #geom_line(data = K.results, aes(x=1:1, y = K1_Med))+
+#  geom_segment(data = K.results, aes(x = 0, xend = 100, y = K.results$K1, yend = K.results$K1), color = "blue")+
+  facet_wrap(~i)+
   theme_bw()
 
 
+
+K1_svals <- seq(0.4, 0.6, length.out = 100)
+
+ll <- rep(NA, 100)
+
+for (i in 1:100){
+  
+  model$setInits(list("K1_scalar" = K1_svals[i]))
+  ll[i] <- calculate(model)
+  
+}
+
+plot(K1_svals, ll)
+abline(v = K1_svals[which.max(ll)])
+abline(v=0.5, col = "red")
